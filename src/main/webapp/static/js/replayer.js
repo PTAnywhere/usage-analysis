8a9bcd37385f155e1d83dfa94eb33d8389f50110
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-10-21 17:40:36 +0100

    Box for showing events with effects.

--- src/main/webapp/static/js/replayer.js
+++ src/main/webapp/static/js/replayer.js
@@ -7,12 +7,14 @@ var replayer = (function () {
     var cbSession;
     var speeds = [1, 2, 4, 8, 16];
 
-    function init(apiPath, cbSessionId, btnSpeedId, btnPlayId, sdrTimelineId) {
+    function init(apiPath, cbSessionId, btnSpeedId, btnPlayId, sdrTimelineId, eventLogId) {
         path = apiPath;
         cbSession = $('#' + cbSessionId);
         btnSpeed.init(btnSpeedId);
-        timeline.init(sdrTimelineId);
+        timeSlider.init(sdrTimelineId);
+        eventLog.init(eventLogId, 4);
         $('#' + btnPlayId).click(function() {
+            eventLog.clear();
             loadRegistration(cbSession.val());
         });
         loadRegistrations();
@@ -28,10 +30,25 @@ var replayer = (function () {
         });
     }
 
+    function printable(statement) {
+        var pieces = statement.verb.id.split('/');
+        var verb = pieces[pieces.length-1];
+        var object = statement.object.id;
+        if ('definition' in statement.object) {
+            if ('name' in statement.object.definition) {
+                object = statement.object.definition.name['en-GB'];
+            }
+        }
+        return verb + ' ' + object;
+    }
+
     function loadRegistration(regId) {
         $.getJSON(path + '/' + regId, function(registration) {
             timeline.set(registration.statements);
-            timeline.play(btnSpeed.get(), function(statement) { console.log(statement); });
+            timeline.play(btnSpeed.get(), function(statement) {
+                                            eventLog.add(printable(statement));
+                                            console.log(statement);
+            });
         }, function(error) {
             errorDialog.open(error);
         });
@@ -71,68 +88,99 @@ var replayer = (function () {
         };
     })();
 
+    var eventLog = (function () {
+        var firstLine, restLines, numLines;
+
+        function init(eventLogId, linesToShow) {
+            numLines = linesToShow;
+            var logDiv = $('#' + eventLogId);
+            logDiv.append('<p class="first"></p><span class="rest"></span>');
+            firstLine = $('.first', logDiv);
+            restLines = $('.rest', logDiv);
+        }
+
+        function addLine(line) {
+            firstLine.hide();
+            restLines.prepend('<p>' + firstLine.text() + '</p>');
+            firstLine.text(line);
+            firstLine.show('slide', {}, 500);
+        }
+
+        function clearLines() {
+            firstLine.text('');
+            restLines.text('');
+        }
+
+        return {
+            init: init,
+            add: addLine,
+            clear: clearLines,
+        };
+    })();
+
+    // If in the final version the user cannot move it, it might be better (more clear) to use a Progressbar.
+    var timeSlider = (function () {
+        /**
+         * A module containing a slider and start and ending labels expressed in "mm:ss".
+         */
+        var elSlider, elStart, elEnd;
+
+        function init(sdrTimelineId) {
+            var sliderDiv = $('#' + sdrTimelineId);  // 1st version: not editable
+            sliderDiv.append('<span class="start">-</span>');
+            sliderDiv.append('<span class="slider">-</span>');
+            sliderDiv.append('<span class="end">-</span>');
+            elSlider = $('.slider', sliderDiv);
+            elStart = $('.start', sliderDiv);
+            elEnd = $('.end', sliderDiv);
+            elSlider.slider({
+                range: "min",
+                slide: function( event, ui ) {
+                    // do nothing
+                    return false;
+                }
+            });
+        }
+
+        function printTime(timestamp) {
+            var relative = new Date(timestamp - min());
+            var secs = relative.getSeconds();
+            return relative.getMinutes() + ':' + ((secs<9)? '0' + secs: secs);
+        }
+
+        function min(value) {
+            if (typeof value === 'undefined')
+                return elSlider.slider('option', 'min'); // getter
+            else elSlider.slider('option', 'min', value);  // setter
+        }
+
+        function max(value) {
+            if (typeof value === 'undefined')
+                return elSlider.slider('option', 'max'); // getter
+            else {
+                elSlider.slider('option', 'max', value);  // setter
+                elEnd.text(printTime(value));
+            }
+        }
+
+        function val(value) {
+            if (typeof value === 'undefined')
+                return elSlider.slider('value'); // getter
+            else {
+                elSlider.slider('value', value);  // setter
+                elStart.text(printTime(value));
+            }
+        }
+
+        return {
+            init: init,
+            min: min,
+            max: max,
+            value: val,
+        };
+    })();
+
     var timeline = (function () {
-        var slider = (function () {
-            /**
-             * A module containing a slider and start and ending labels expressed in "mm:ss".
-             */
-            var elSlider, elStart, elEnd;
-
-            function init(sdrTimelineId) {
-                var sliderDiv = $('#' + sdrTimelineId);  // 1st version: not editable
-                sliderDiv.append('<span class="start">-</span>');
-                sliderDiv.append('<span class="slider">-</span>');
-                sliderDiv.append('<span class="end">-</span>');
-                elSlider = $('.slider', sliderDiv);
-                elStart = $('.start', sliderDiv);
-                elEnd = $('.end', sliderDiv);
-                elSlider.slider({
-                    range: "min",
-                    slide: function( event, ui ) {
-                        // do nothing
-                        return false;
-                    }
-                });
-            }
-
-            function printTime(timestamp) {
-                var relative = new Date(timestamp - min());
-                var secs = relative.getSeconds();
-                return relative.getMinutes() + ':' + ((secs<9)? '0' + secs: secs);
-            }
-
-            function min(value) {
-                if (typeof value === 'undefined')
-                    return elSlider.slider('option', 'min'); // getter
-                else elSlider.slider('option', 'min', value);  // setter
-            }
-
-            function max(value) {
-                if (typeof value === 'undefined')
-                    return elSlider.slider('option', 'max'); // getter
-                else {
-                    elSlider.slider('option', 'max', value);  // setter
-                    elEnd.text(printTime(value));
-                }
-            }
-
-            function val(value) {
-                if (typeof value === 'undefined')
-                    return elSlider.slider('value'); // getter
-                else {
-                    elSlider.slider('value', value);  // setter
-                    elStart.text(printTime(value));
-                }
-            }
-
-            return {
-                init: init,
-                min: min,
-                max: max,
-                value: val,
-            };
-        })();
-
         var stmtIndex;
         var statements;
         var stepsPerSecond;
@@ -143,9 +191,9 @@ var replayer = (function () {
             var start = new Date(statements[0].timestamp);
             var end = new Date(statements[statements.length-1].timestamp);
             startTime = start.getTime();
-            slider.min(startTime);
-            slider.max(end.getTime());
-            slider.value(startTime);
+            timeSlider.min(startTime);
+            timeSlider.max(end.getTime());
+            timeSlider.value(startTime);
         }
 
         function getCurrentEventTimestamp() {
@@ -167,14 +215,14 @@ var replayer = (function () {
         }
 
         function updateSlider(callback) {
-            var next = slider.value() + stepsPerSecond;
-            var max = slider.max();
+            var next = timeSlider.value() + stepsPerSecond;
+            var max = timeSlider.max();
             if (next > max) {
-                slider.value(max);
+                timeSlider.value(max);
                 dispatchEventsUntil(max, callback);
                 //console.log('Nothing else to update');
             } else {
-                slider.value(next);
+                timeSlider.value(next);
                 dispatchEventsUntil(next, callback);
                 setTimeout(updateSlider, 1000, callback);
             }
@@ -182,7 +230,7 @@ var replayer = (function () {
 
         function play(startingAt, speed, callback) {
             stepsPerSecond = speed * 1000;  // x1 1 second each second, x2 2 seconds every second, etc.
-            slider.value(startTime);
+            timeSlider.value(startTime);
             stmtIndex = 0;
             setTimeout(updateSlider, 1000, callback);  // Updates slider every second
         }
@@ -192,7 +240,6 @@ var replayer = (function () {
         }
 
         return {
-            init: slider.init,
             set: setStatements,
             play: playFromBeginning
         };
@@ -200,7 +247,7 @@ var replayer = (function () {
 
     return {
         create: init,
-        load: loadRegistration
+        load: loadRegistration,
     };
 
 })();
