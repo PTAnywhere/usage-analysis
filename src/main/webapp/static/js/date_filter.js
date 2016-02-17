71890225bef14f2935dafeef770a4304e96a2fc1
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2016-02-17 15:26:20 +0000

    Moving calendar filter logic to an external JS file.
    The intention is to reuse this code in the different visualizations.

---
+++ src/main/webapp/static/js/date_filter.js
@@ -0,0 +1,57 @@
+
+var timeFilter = (function () {
+    var startEl, endEl;
+
+    function getStartDate() {
+        return startEl.data("DateTimePicker").date();
+    }
+
+    function setStartDate(timeMoment) {
+        startEl.data("DateTimePicker").date(moment('2016-01-27').startOf('day'));
+    }
+
+    function setEndDate(timeMoment) {
+        endEl.data("DateTimePicker").date(moment('2016-01-28').endOf('day'));
+    }
+
+    function configure(startId, endId) {
+        startEl = $('#' + startId);
+        endEl = $('#' + endId);
+
+        startEl.datetimepicker({
+            format: dateFormat,
+            useCurrent: false,
+            defaultDate: moment().startOf('day'),
+        });
+
+        endEl.datetimepicker({
+            format: dateFormat,
+            useCurrent: false,
+            defaultDate: moment().endOf('day'),
+        });
+
+        startEl.on('dp.change', function(e) {
+            endEl.data("DateTimePicker").minDate(e.date.add(1, 'hours'));
+        });
+
+        endEl.on('dp.change', function(e) {
+            var startDate = getStartDate();
+            if (e.date<startDate) {
+                endEl.data("DateTimePicker").date(startDate.add(1, 'days'));
+            }
+        });
+    }
+
+    function getURLParams() {
+        var startISO = $('#startTime').data("DateTimePicker").date().toISOString();
+        var endISO = $('#endTime').data("DateTimePicker").date().toISOString();
+        return 'start=' + startISO +'&end=' + endISO;
+    }
+
+    return {
+        configure: configure,
+        setStartDate: setStartDate,
+        setEndDate: setEndDate,
+        getURLParameters: getURLParams,
+    };
+})();
