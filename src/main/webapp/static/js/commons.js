0dda09c084eafda7f4a6434be480915a398802e8
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2016-03-11 14:33:14 +0000

    Adding page to list actions inside a session.

--- src/main/webapp/static/js/commons.js
+++ src/main/webapp/static/js/commons.js
@@ -1,7 +1,7 @@
 
 
 var queryParameter = (function () {
-  function getParameter(name) {
+    function getParameter(name) {
     var lookFor = name + '=';
     var sessionParamStart = window.location.href.indexOf(lookFor);
     var ret = null;
@@ -13,18 +13,18 @@ var queryParameter = (function () {
         }
     }
     return ret;
-  }
+    }
 
-  function getAllParameters() {
+    function getAllParameters() {
       var paramPos = window.location.href.indexOf('?');
       if (paramPos==-1) return null;
       return window.location.href.substr(paramPos + 1);
-  }
+    }
 
-  return {
+    return {
       get: getParameter,
       all: getAllParameters
-  };
+    };
 })();
 
 
@@ -45,16 +45,25 @@ var timeFilter = (function () {
     var displayDateFormat = 'MM/DD/YYYY HH:00';
     var startEl, endEl;
 
-    function getStartDate() {
-        return startEl.data("DateTimePicker").date();
+    function getEndDate() {
+        return endEl.data("DateTimePicker").date();
     }
 
     function setStartDate(timeMoment) {
-        startEl.data("DateTimePicker").date(moment('2016-01-27').startOf('day'));
+        startEl.data("DateTimePicker").date(timeMoment);
     }
 
     function setEndDate(timeMoment) {
-        endEl.data("DateTimePicker").date(moment('2016-01-28').endOf('day'));
+        endEl.data("DateTimePicker").date(timeMoment);
+    }
+
+    function setMinEndDate(timeMoment) {
+        var minMoment = timeMoment.add(1, 'hours');
+
+        endEl.data("DateTimePicker").minDate(minMoment);
+        if (getEndDate() < minMoment) {
+            setEndDate(minMoment);
+        }
     }
 
     function configure(startId, endId) {
@@ -74,14 +83,7 @@ var timeFilter = (function () {
         });
 
         startEl.on('dp.change', function(e) {
-            endEl.data("DateTimePicker").minDate(e.date.add(1, 'hours'));
-        });
-
-        endEl.on('dp.change', function(e) {
-            var startDate = getStartDate();
-            if (e.date<startDate) {
-                endEl.data("DateTimePicker").date(startDate.add(1, 'days'));
-            }
+            setMinEndDate(e.date);
         });
     }
 
@@ -96,5 +98,160 @@ var timeFilter = (function () {
         setStartDate: setStartDate,
         setEndDate: setEndDate,
         getURLParameters: getURLParams,
+    };
+})();
+
+
+var statements = (function () {
+    function createListener() {
+        return new Listener();
+    }
+
+    function Listener() {
+        this.callbacks = {
+            deviceCreation: null,
+            deviceRemoval: null,
+            deviceUpdate: null,
+            linkCreation: null,
+            linkRemoval: null,
+            openCmd: null,
+            closeCmd: null,
+            useCmd: null,
+            readCmd: null
+        };
+    }
+
+    Listener.prototype.onCreateDevice = function(creationCallback) {
+        this.callbacks.deviceCreation = creationCallback;
+        return this;
+    }
+
+    Listener.prototype.onDeleteDevice = function(removalCallback) {
+        this.callbacks.deviceRemoval = removalCallback;
+        return this;
+    }
+
+    Listener.prototype.onUpdateDevice = function(updateCallback) {
+        this.callbacks.deviceUpdate = updateCallback;
+        return this;
+    }
+
+    Listener.prototype.onUpdatePort = function(updateCallback) {
+        this.callbacks.portUpdate = updateCallback;
+        return this;
+    }
+
+    Listener.prototype.onCreateLink = function(creationCallback) {
+        this.callbacks.linkCreation = creationCallback;
+        return this;
+    }
+
+    Listener.prototype.onDeleteLink = function(removalCallback) {
+        this.callbacks.linkRemoval = removalCallback;
+        return this;
+    }
+
+    Listener.prototype.onOpenCommandLine = function(openCallback) {
+        this.callbacks.openCmd = openCallback;
+        return this;
+    }
+
+    Listener.prototype.onCloseCommandLine = function(closeCallback) {
+        this.callbacks.closeCmd = closeCallback;
+        return this;
+    }
+
+    Listener.prototype.onUseCommandLine = function(useCallback) {
+        this.callbacks.useCmd = useCallback;
+        return this;
+    }
+
+    Listener.prototype.onReadCommandLine = function(readCallback) {
+        this.callbacks.readCmd = readCallback;
+        return this;
+    }
+
+    Listener.prototype.onStatement = function(statement) {
+        var dext = (statement.result===undefined)? null: statement.result.extensions;
+        if (statement.verb.id===verb.create) {
+            if (statement.object.id.startsWith(devices.any)) {
+                if (this.callbacks.deviceCreation==null) {
+                    console.warn('No callback function defined for device creation.');
+                } else {
+                    this.callbacks.deviceCreation(dext);
+                }
+            } else if (statement.object.id == activity.link) {
+                if (this.callbacks.linkCreation==null) {
+                    console.warn('No callback function defined for link creation.');
+                } else {
+                    this.callbacks.linkCreation(dext);
+                }
+            }
+        } else if (statement.verb.id===verb.delete) {
+            if (statement.object.id.startsWith(devices.any)) {
+                if (this.callbacks.deviceRemoval==null) {
+                    console.warn('No callback function defined for device removal.');
+                } else {
+                    this.callbacks.deviceRemoval(dext);
+                }
+            } else if (statement.object.id == activity.link) {
+                if (this.callbacks.linkRemoval==null) {
+                    console.warn('No callback function defined for link removal.');
+                } else {
+                    this.callbacks.linkRemoval(dext);
+                }
+            }
+        } else if (statement.verb.id===verb.update) {
+            if (statement.object.id.startsWith(devices.any)) {
+                if (this.callbacks.deviceUpdate==null) {
+                    console.warn('No callback function defined for device update.');
+                } else {
+                    this.callbacks.deviceUpdate(dext, statement.result.response);
+                }
+            } else if (statement.object.definition.type==activity.port) {
+                if (this.callbacks.portUpdate==null) {
+                    console.warn('No callback function defined for device port update.');
+                } else {
+                    this.callbacks.portUpdate(dext);
+                }
+            }
+        } else if (statement.verb.id===verb.opened) {
+            if (statement.object.definition.type==activity.commandLine) {
+                if (this.callbacks.openCmd==null) {
+                    console.warn('No callback function defined for command line open.');
+                } else {
+                    // It would be better to store the name as an extension too.
+                    this.callbacks.openCmd(statement.object.definition.name['en-GB']);
+                }
+            }
+        } else if (statement.verb.id===verb.closed) {
+            if (statement.object.definition.type==activity.commandLine) {
+                if (this.callbacks.closeCmd==null) {
+                    console.warn('No callback function defined for command line close.');
+                } else {
+                    this.callbacks.closeCmd();
+                }
+            }
+        } else if (statement.verb.id===verb.use) {
+            if (statement.object.definition.type==activity.commandLine) {
+                if (this.callbacks.useCmd==null) {
+                    console.warn('No callback function defined for command line use.');
+                } else {
+                    this.callbacks.useCmd(dext);
+                }
+            }
+        } else if (statement.verb.id===verb.read) {
+            if (statement.object.definition.type==activity.commandLine) {
+                if (this.callbacks.readCmd==null) {
+                    console.warn('No callback function defined for command line read.');
+                } else {
+                    this.callbacks.readCmd(statement.result.response);
+                }
+            }
+        }
+    }
+
+    return {
+        newListener: createListener
     };
 })();
